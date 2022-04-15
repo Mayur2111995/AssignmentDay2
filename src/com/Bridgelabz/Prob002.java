3a330c2f2c4fe4f0d376c830e03074d9178083af
Mayur2111995 <barimayur1265@gmail.com>
2022-04-15 23:13:03 +0530

    Added:integerNumber

---
+++ src/com/Bridgelabz/Prob002.java
@@ -0,0 +1,18 @@
+package com.Bridgelabz;
+import java.util.Scanner;
+public class Prob002 {
+    public static void main(String [] args){
+        //Write a Program to reverse the integer number eg. Input n=231 reverse is 132
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number=");
+        int number = scanner.nextInt(); // 123
+        int reverse = 0;
+        while (number != 0) {
+            int lastDigit = number % 10;
+            reverse = reverse * 10 + lastDigit;
+            number = number / 10;
+        }
+        System.out.println(reverse);
+    }
+    }
+
